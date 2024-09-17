3cbf9910f5001aee6c09330ff84a59eb88b456e5
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-17 09:44:23 +0000

    feat: Add additional criteria to sort/filter blog posts - Add further django logic in the blog veiws.py to allow for additional filter/sorting options - Add HTML to blog.html to include the additional options within the filter posts section - Add initial styling for responsiveness of filter posts section

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,26 @@ from .models import BlogPost
 
 # Show blog.html content
 def blog(request):
+    category = request.GET.get('category')
+    author = request.GET.get('author')
+    sort = request.GET.get('sort')
+
     blog_posts = BlogPost.objects.all()
+
+    if category:
+        blog_posts = blog_posts.filter(category=category)
+    if author:
+        blog_posts = blog_posts.filter(author__username__icontains=author)
+
+    if sort == 'published_date_desc':
+        blog_posts = blog_posts.order_by('-published_date')
+    elif sort == 'published_date_asc':
+        blog_posts = blog_posts.order_by('published_date')
+    elif sort == 'popularity_desc':
+        blog_posts = blog_posts.order_by('-likes')
+    elif sort == 'popularity_asc':
+        blog_posts = blog_posts.order_by('likes')
+        
     return render(request, 'blog.html', {'blog_posts': blog_posts})
 
 # Obtain the initial top 5 popular posts
