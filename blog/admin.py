fdf54d89f332b2a2bb006896263cf722e005ba25
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-17 12:10:10 +0000

    feat: enable Superuser deletion of blog posts in admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'likes')
+    list_filter = ('author', 'published_date')
+    search_fields = ('title', 'content')
+
+admin.site.register(BlogPost, BlogPostAdmin)
