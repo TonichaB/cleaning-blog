0e727442f66cc5822bf87f6041f7c7b5ffdc16d7
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-22 20:37:27 +0000

    feat: create new Comment model for comments, nested comments, likes on comments and supported media (emojis & gifs)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# Blog Posts Model
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -17,9 +19,25 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
+# Likes Model
+
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'blog_post')
+
+# Comments Model
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    content = models.TextField()
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
