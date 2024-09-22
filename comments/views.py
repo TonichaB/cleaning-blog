cdd1f579c81c29ccdefa7b42b8e22b493eeff37b
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-22 20:57:10 +0000

    feat: create comments views for adding new comments and liking comments

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from .models import Comment
+from blog.models import BlogPost
 
 # Create your views here.
 
-def comments(request):
-    return HttpResponse("Hello World 3")
+# Adding New Comments/Replies
+
+def add_comment(request, post_id):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        parent_id = request.POST.get('parent_id', None)
+        post = get_object_or_404(BlogPost, id=post_id)
+
+        if parent_id:
+            parent = Comment.objects.get(id=parent_id)
+            comment = Comment.objects.create(user=request.user, post=post, content=content, parent=parent)
+        else:
+            comment = Comment.objects.create(user=request.user, post=post, content=content,)
+    
+    return JsonResponse({'success': True, 'message': 'Comment added', 'comment_id': comment_id})
+
+# Like/Unlike a Comment
+
+def like_comment(request):
+    if request.method == 'POST':
+        comment_id = request.POST.get('comment_id')
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user in comment.likes.all():
+            comment.likes.remove(request.user)
+            liked = False
+        else:
+            comment.likes.add(request.user)
+            liked = True
+        return JsonResponse({'success': True, 'liked': liked, 'likes_count': comment.likes.count()})
