74ed4460086e1c9b678d61785186a166259bc41b
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-09-24 23:38:29 +0000

    feat: update django admin to include comments, allowing admin users to delete comments

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Comment
 
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'content', 'created_at')
+    search_fields = ('user__username', 'content', 'post__title')
+    list_filter = ('created_at', 'user')
+
+admin.site.register(Comment, CommentAdmin)
