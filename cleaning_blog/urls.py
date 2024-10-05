f88430ecc5647badcbbb9b4c02181658ea01111d
Tonicha Blackwell <tonicha.blackwell97@gmail.com>
2024-10-05 21:58:58 +0000

    refactor: adjust urls.py format to pass validation

--- cleaning_blog/urls.py
+++ cleaning_blog/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import home, about, contact
-from blog.views import blog, top_posts, get_popular_posts, user_post_detail, user_posts_api, create_post_view, edit_post, delete_post, like_post, my_posts
+from blog.views import (
+    blog,
+    top_posts,
+    get_popular_posts,
+    user_post_detail,
+    user_posts_api,
+    create_post_view,
+    edit_post,
+    delete_post,
+    like_post,
+    my_posts,
+)
 from comments.views import add_comment, load_comments
 from users.views import register_view, login_view, logout_view
 from django.conf import settings
@@ -9,14 +20,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), # Home Page
-    path('about/', about, name='about'), # About Page
-    path('contact/', contact, name='contact'), # Contact Page
-    path('blog/', blog, name='blog'), # Blog Page
+    path('', home, name='home'),  # Home Page
+    path('about/', about, name='about'),  # About Page
+    path('contact/', contact, name='contact'),  # Contact Page
+    path('blog/', blog, name='blog'),  # Blog Page
     path('top-posts/', top_posts, name='top_posts'),
     path('get-popular-posts/', get_popular_posts, name='get_popular_posts'),
     path('user-posts-api/', user_posts_api, name='user_posts_api'),
-    path('user-post-detail/<int:post_id>/', user_post_detail, name='user_post_detail'),
+    path(
+        'user-post-detail/<int:post_id>/',
+        user_post_detail,
+        name='user_post_detail'
+        ),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
